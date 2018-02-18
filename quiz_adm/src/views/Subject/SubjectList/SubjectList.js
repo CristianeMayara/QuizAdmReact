14a5ba9f5de01da5624470108f094cdabbd2190e
Cristiane Mayara <cris.mayara@gmail.com>
2018-02-18 19:57:22 -0300

    Adding the Subject List option in the Sidebar and setting a new route

--- quiz_adm/src/views/Subject/SubjectList/SubjectList.js
+++ quiz_adm/src/views/Subject/SubjectList/SubjectList.js
@@ -5,6 +5,12 @@ class SubjectList extends Component {
   constructor(props) {
     super(props)
   }
+
+  render() {
+    return (
+      <h1>Subject List</h1>
+    )
+  }
 }
 
 export default SubjectList;
