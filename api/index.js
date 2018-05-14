ace8c204503af04a074e7ce21baf550e1c17a933
Cristiane Mayara <cris.mayara@gmail.com>
2018-05-13 23:04:35 -0300

    Adding server

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, HOST, () => console.log(`Express has been started at ${HOST}:${PORT}.`));
