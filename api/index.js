aa19745ad625d57c68d78c6a321d4f2469b8e525
Cristiane Mayara <cris.mayara@gmail.com>
2018-05-13 23:32:33 -0300

    Adding routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import routes from './src/index';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,5 +12,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+routes(app);
 
 app.listen(PORT, HOST, () => console.log(`Express has been started at ${HOST}:${PORT}.`));
---
+++ api/src/subjects/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import list from './list';
+
+const router = express.Router();
+router.get('/', list);
+
+export default router;
