9d90c19624ddc63f2c8d7239c42c52ac0dc10e12
Ferreira <cristiane.ferreira@accenture.com>
2018-10-26 00:55:54 -0300

    Adding create endpoint

--- api/src/subjects/index.js
+++ api/src/subjects/index.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import list from './list';
+import express from "express";
+import list from "./list";
+import create from "./create";
 
 const router = express.Router();
-router.get('/', list);
+router.get("/", list);
+router.post("/", create);
 
 export default router;
