97c9a28c7fd32d921a2f01b12187921c00891b47
Ferreira <cristiane.ferreira@accenture.com>
2018-10-30 23:18:24 -0300

    Using action and reducer to create a new Subject by the web app

--- quiz_adm/src/views/Subject/SubjectCreate/SubjectCreate.js
+++ quiz_adm/src/views/Subject/SubjectCreate/SubjectCreate.js
@@ -1,57 +1,110 @@
-import React, { Component } from 'react';
-import { Row, Col, Card, CardHeader, CardBody, CardFooter, Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import { connect } from 'react-redux';
-
+import React, { Component } from "react";
+import {
+  Col,
+  Row,
+  Card,
+  Form,
+  Input,
+  Label,
+  Button,
+  CardBody,
+  FormGroup,
+  CardFooter,
+  CardHeader
+} from "reactstrap";
+import { connect } from "react-redux";
+import { thunkCreateSubject } from "../../../actions/Subject";
 
 class SubjectCreate extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      subject: {
+        name: "",
+        description: ""
+      }
+    };
+
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+  }
+
+  handleCreate(subject) {
+    return this.props.createSubject(subject);
+  }
+
+  handleChangeInput(event) {
+    const { target } = event;
+    const { value, name } = target;
+
+    let { subject } = this.state;
+    subject[name] = value;
+
+    return this.setState({ subject });
   }
 
   render() {
+    console.log(this.state);
     return (
-      <div className='animated fadeIn'>
+      <div className="animated fadeIn">
         <Row>
-          <Col xs='12' md='12'>
+          <Col xs="12" md="12">
             <Card>
-              <CardHeader>
-                Listing
-                </CardHeader>
+              <CardHeader>Listing</CardHeader>
               <CardBody>
                 <Form>
                   <FormGroup>
-                    <Label htmlFor='name'>Subject Name:</Label>
-                    <Input type='text' name='name' placeholder='Enter subject name'></Input>
+                    <Label htmlFor="name">Subject Name:</Label>
+                    <Input
+                      name="name"
+                      type="text"
+                      placeholder="Enter subject name"
+                      onChange={this.handleChangeInput}
+                    />
                   </FormGroup>
                   <FormGroup>
-                    <Label htmlFor='description'>Subject Description:</Label>
-                    <Input type='textarea' name='name' placeholder='Enter subject description'></Input>
+                    <Label htmlFor="description">Subject Description:</Label>
+                    <Input
+                      name="description"
+                      type="textarea"
+                      onChange={this.handleChangeInput}
+                      placeholder="Enter subject description"
+                    />
                   </FormGroup>
                 </Form>
               </CardBody>
               <CardFooter>
-                <Button type='button' color='primary'>Create</Button>
+                <Button
+                  type="button"
+                  color="primary"
+                  onClick={() => this.handleCreate(this.state.subject)}
+                >
+                  Create
+                </Button>
               </CardFooter>
             </Card>
           </Col>
         </Row>
       </div>
-
-    )
+    );
   }
 }
 
-const mapStateToPropos = (state) => {
+const mapStateToProps = state => {
   return {
+    subject: state.subjectStore.subject,
+    error: state.subjectStore.error
+  };
+};
 
-  }
-}
-
-const mapDispathToProps = (dispath) => {
+const mapDispathToProps = dispatch => {
   return {
+    createSubject: subject => dispatch(thunkCreateSubject(subject))
+  };
+};
 
-  }
-}
-
-export default connect(mapStateToPropos, mapDispathToProps)(SubjectCreate);
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(SubjectCreate);
