46e500a138005327e6ad4a02ead7f74446ae9e99
Ferreira <cristiane.ferreira@accenture.com>
2018-11-02 22:01:08 -0300

    Adding service to Find a user

--- api/src/users/show.js
+++ api/src/users/show.js
@@ -1,1 +1,10 @@
-export default async (req, res) => {};
+import User from "../models/User";
+
+export default async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
+    return res.json({ user });
+  } catch (err) {
+    return res.status(500).json({ err });
+  }
+};
