589e331aa883b741a1ea4d3d9b947ec6d3dff4a7
Ferreira <cristiane.ferreira@accenture.com>
2018-11-02 21:54:40 -0300

    Adding service to List users

--- api/src/users/index.js
+++ api/src/users/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import create from "./create";
+import list from "./list";
+import show from "./show";
 
 const router = express.Router();
 
 router.post("/", create);
+router.get("/", list);
+router.get("/:id", show);
 
 export default router;
